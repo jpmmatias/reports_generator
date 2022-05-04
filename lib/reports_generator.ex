a92943e57ff8fc5376f90bcfb52f87fcd192bf02
João Pedro Matias <joao.matias@rebase.com.br>
2022-05-04 14:39:51 -0300

    Finalizing project

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,19 +1,44 @@
 defmodule ReportsGenerator do
+  alias ReportsGenerator.Parser
+
+  @available_foods [
+    "açaí",
+    "churrasco",
+    "esfirra",
+    "hambúrguer",
+    "pastel",
+    "pizza",
+    "prato_feito",
+    "sushi"
+  ]
+
+  @options ["foods", "users"]
+
   def build(filename) do
-    "reports/#{filename}"
-    |> File.stream!()
-    |> Enum.reduce(sum_values_per_user, fn line, report ->
-      [id, _food_name, price] = parse_line(line)
-      Map.put(report, id, report[id] + price)
-    end)
+    filename
+    |> ReportsGenerator.Parser.parse_file()
+    |> Enum.reduce(reports_acc(), fn line, report -> sum_values_per_user(line, report) end)
   end
 
-  defp parse_line(line) do
-    line
-    |> String.trim()
-    |> String.split(",")
-    |> List.update_at(2, &String.to_integer/1)
+  def fetch_higher_cost(report, option) when option in @options,
+    do: {:ok, Enum.max_by(report[option], fn {_key, value} -> value end)}
+
+  def fetch_higher_cost(_report, _option), do: {:error, "Invalid option!"}
+
+  defp reports_acc do
+    foods = Enum.into(@available_foods, %{}, &{&1, 0})
+    users = Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
+
+    %{"users" => users, "foods" => foods}
   end
 
-  defp sum_values_per_user, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
+  defp sum_values_per_user(
+         [id, food_name, price],
+         %{"foods" => foods, "users" => users} = report
+       ) do
+    users = Map.put(users, id, users[id] + price)
+    foods = Map.put(foods, food_name, foods[food_name] + 1)
+
+    %{report | "users" => users, "foods" => foods}
+  end
 end
--- test/reports_generator_test.exs
+++ test/reports_generator_test.exs
@@ -2,7 +2,99 @@ defmodule ReportsGeneratorTest do
   use ExUnit.Case
   doctest ReportsGenerator
 
-  test "greets the world" do
-    assert ReportsGenerator.hello() == :world
+  describe "build/1" do
+    test "Build general report" do
+      file_name = "report_test.csv"
+
+      expected_response = %{
+        "foods" => %{
+          "açaí" => 1,
+          "churrasco" => 2,
+          "esfirra" => 3,
+          "hambúrguer" => 2,
+          "pastel" => 0,
+          "pizza" => 2,
+          "prato_feito" => 0,
+          "sushi" => 0
+        },
+        "users" => %{
+          "1" => 48,
+          "10" => 36,
+          "11" => 0,
+          "12" => 0,
+          "13" => 0,
+          "14" => 0,
+          "15" => 0,
+          "16" => 0,
+          "17" => 0,
+          "18" => 0,
+          "19" => 0,
+          "2" => 45,
+          "20" => 0,
+          "21" => 0,
+          "22" => 0,
+          "23" => 0,
+          "24" => 0,
+          "25" => 0,
+          "26" => 0,
+          "27" => 0,
+          "28" => 0,
+          "29" => 0,
+          "3" => 31,
+          "30" => 0,
+          "4" => 42,
+          "5" => 49,
+          "6" => 18,
+          "7" => 27,
+          "8" => 25,
+          "9" => 24
+        }
+      }
+
+      response = ReportsGenerator.build(file_name)
+
+      assert response == expected_response
+    end
+  end
+
+  describe "fetch_higher_cost/2" do
+    test "Fetch higher cost from users" do
+      file_name = "report_test.csv"
+
+      response =
+        file_name
+        |> ReportsGenerator.build()
+        |> ReportsGenerator.fetch_higher_cost("users")
+
+      expected_response = {:ok, {"5", 49}}
+
+      expected_response = assert response == expected_response
+    end
+
+    test "Fetch higher cost from foods" do
+      file_name = "report_test.csv"
+
+      response =
+        file_name
+        |> ReportsGenerator.build()
+        |> ReportsGenerator.fetch_higher_cost("foods")
+
+      expected_response = {:ok, {"esfirra", 3}}
+
+      expected_response = assert response == expected_response
+    end
+
+    test "Fetch higher cost when is invalid option" do
+      file_name = "report_test.csv"
+
+      response =
+        file_name
+        |> ReportsGenerator.build()
+        |> ReportsGenerator.fetch_higher_cost("invalid_option")
+
+      expected_response = {:error, "Invalid option!"}
+
+      expected_response = assert response == expected_response
+    end
   end
 end
