949e8e495e706d3cae483ecabdeba3787e5609c4
João Pedro Matias <joao.matias@rebase.com.br>
2022-05-09 15:05:50 -0300

    Finalized project

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -20,6 +20,12 @@ defmodule ReportsGenerator do
     |> Enum.reduce(reports_acc(), fn line, report -> sum_values_per_user(line, report) end)
   end
 
+  def build_from_many(filenames) do
+    filenames
+    |> Task.async_stream(&build/1)
+    |> Enum.reduce(reports_acc(), fn {:ok, result}, report -> sum_reports(report, result) end)
+  end
+
   def fetch_higher_cost(report, option) when option in @options,
     do: {:ok, Enum.max_by(report[option], fn {_key, value} -> value end)}
 
@@ -30,6 +36,17 @@ defmodule ReportsGenerator do
     users = Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 
     %{"users" => users, "foods" => foods}
+  end
+
+  defp sum_reports(%{"foods" => foods1, "users"=> users1}, %{"foods"=> foods2, "users" => users2}) do
+    foods = merge_maps_and_sum(foods1, foods2)
+    users = merge_maps_and_sum(users1, users2)
+
+    %{"foods"=> foods, "users"=>users}
+  end
+
+  defp merge_maps_and_sum(map1,map2) do
+    Map.merge(map1,map2, fn _key, value1,value2 -> value1+value2)
   end
 
   defp sum_values_per_user(
